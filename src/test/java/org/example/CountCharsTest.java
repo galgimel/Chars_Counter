0b4dccadb2ff495278209a0fddff6a9b83fbd33a
galgimel <samokhina.galya.15@gmail.com>
2022-10-24 16:16:48 +0300

    Test CountChars and Formatter

--- src/test/java/org/example/CountCharsTest.java
+++ src/test/java/org/example/CountCharsTest.java
@@ -13,16 +13,10 @@ class CountCharsTest {
     void countCharsTestEmptySpase() {
         CountChars counter = new CountChars();
         Map<Character, Integer> expected = new HashMap<>();
-
         Map<Character, Integer> actual = counter.countChars("");
 
         assertEquals(expected, actual);
-    }
 
-    @Test
-    void countCharsTestRegister() {
-        CountChars counter = new CountChars();
-        Map<Character, Integer> expected = new HashMap<>();
         expected.put(' ',1);
         expected.put('h',1);
         expected.put('e',1);
@@ -33,20 +27,19 @@ class CountCharsTest {
         expected.put('L',2);
         expected.put('O',1);
 
-        Map<Character, Integer> actual = counter.countChars("hello HELLO");
+        actual = counter.countChars("hello HELLO");
 
         assertEquals(expected, actual);
-    }
 
-    @Test
-    void countCharsALotOfChars() {
-        CountChars counter = new CountChars();
-        Map<Character, Integer> expected = new HashMap<>();
+        expected.clear();
+        actual.clear();
+
         expected.put('a',18);
         expected.put(' ',1);
 
-        Map<Character, Integer> actual = counter.countChars("aaaaaaaaa aaaaaaaaa");
+        actual = counter.countChars("aaaaaaaaa aaaaaaaaa");
 
         assertEquals(expected, actual);
+
     }
 }
--- src/test/java/org/example/FormatterTest.java
+++ src/test/java/org/example/FormatterTest.java
@@ -17,30 +17,21 @@ class FormatterTest {
         String actual = formatter.format(testMap);
 
         assertEquals(expected, actual);
-    }
 
-    @Test
-    void formatTestWord() {
-        Formatter formatter = new Formatter();
-        Map<Character, Integer> testMap = new HashMap<>();
         testMap.put('h',1);
         testMap.put('e',1);
         testMap.put('l',2);
         testMap.put('o',1);
 
-        String expected = "e : 1\n" + "h : 1\n" + "l : 2\n" + "o : 1\n";
-        String actual = formatter.format(testMap);
+        expected = "e : 1\n" + "h : 1\n" + "l : 2\n" + "o : 1\n";
+        actual = formatter.format(testMap);
 
         assertEquals(expected, actual);
-    }
+        testMap.clear();
 
-    @Test
-    void formatTestALotOfChars() {
-        Formatter formatter = new Formatter();
-        Map<Character, Integer> testMap = new HashMap<>();
         testMap.put('a', 10);
-        String expected = "a : 10\n";
-        String actual = formatter.format(testMap);
+        expected = "a : 10\n";
+        actual = formatter.format(testMap);
 
         assertEquals(expected, actual);
     }
